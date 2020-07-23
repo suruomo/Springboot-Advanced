506f7f50597b2c44f509bf2fb1b87766d0167272
suruomo <www.suruomo@hotmail.com>
2020-07-23 13:27:09 +0800

    springsecurity
    登陆认证、授权，记住我

---
+++ springboot-05-security/src/test/java/com/suruomo/security/Springboot05SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suruomo.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot05SecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

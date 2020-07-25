90f06ed27b69b67b1885df1d9abb2cbb948b8faf
suruomo <www.suruomo@hotmail.com>
2020-07-25 09:22:16 +0800

    actuator

---
+++ springboot-08-actuator/src/main/java/com/suruomo/actuator/Springboot08ActuatorApplication.java
@@ -0,0 +1,16 @@
+package com.suruomo.actuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 苏若墨
+ */
+@SpringBootApplication
+public class Springboot08ActuatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot08ActuatorApplication.class, args);
+    }
+
+}
---
+++ springboot-08-actuator/src/test/java/com/suruomo/actuator/Springboot08ActuatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suruomo.actuator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot08ActuatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

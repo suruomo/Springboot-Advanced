594c4fc2127643a0cc0ecadd0f3abfafe7a7a604
suruomo <www.suruomo@hotmail.com>
2020-07-27 20:36:44 +0800

    修改：全局异常处理
    返回定制错误页面

--- springboot-09-exception/src/main/java/com/suruomo/springboot09exception/exception/GlobalExceptionHandler.java
+++ springboot-09-exception/src/main/java/com/suruomo/springboot09exception/exception/GlobalExceptionHandler.java
@@ -1,9 +1,11 @@
 package com.suruomo.springboot09exception.exception;
 
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author suruomo
@@ -12,28 +14,42 @@ import org.springframework.web.bind.annotation.ResponseBody;
  */
 // 指定捕捉特定类异常 @ControllerAdvice(assignableTypes = {ExceptionController.class})
 @ControllerAdvice
-@ResponseBody
 public class GlobalExceptionHandler {
-    ErrorResponse illegalArgumentResponse = new ErrorResponse(new IllegalArgumentException("参数错误!"));
-    ErrorResponse NullPointerException = new ErrorResponse(new NullPointerException("错误：空对象！"));
+
+    private Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+    ErrorResponse illegalArgumentException  = new ErrorResponse(new IllegalArgumentException("请求参数错误，请稍后重试!"));
+    ErrorResponse NullPointerException = new ErrorResponse(new NullPointerException("请求发生了空指针异常，请稍后再试"));
 
     /**
+     *
      * 拦截非法参数异常
-     * @param e
+     * @param request
      * @return
+     * @throws Exception
      */
     @ExceptionHandler(value = IllegalArgumentException.class)
-    public ResponseEntity<ErrorResponse> illegalArgumentExceptionHandler(Exception e) {
-            return ResponseEntity.status(400).body(illegalArgumentResponse);
+    public ModelAndView illegalArgumentExceptionHandler(HttpServletRequest request) throws Exception {
+        log.error("Requst URL : {}，Exception : {}", request.getRequestURL(),illegalArgumentException.getMessage());
+        ModelAndView mv = new ModelAndView();
+        mv.addObject("msg",illegalArgumentException.getMessage());
+        mv.addObject("type", illegalArgumentException.getErrorTypeName());
+        mv.setViewName("error");
+        return mv;
     }
 
     /**
      * 拦截空指针异常
-     * @param e
+     * @param request
      * @return
      */
     @ExceptionHandler(value = NullPointerException.class)
-    public ResponseEntity<ErrorResponse> nullPointerExceptionHandler(Exception e) {
-        return ResponseEntity.status(500).body(NullPointerException);
+    public ModelAndView nullPointerExceptionHandler(HttpServletRequest request) {
+        log.error("Requst URL : {}，Exception : {}", request.getRequestURL(),NullPointerException.getMessage());
+        ModelAndView mv = new ModelAndView();
+        mv.addObject("msg",NullPointerException.getMessage());
+        mv.addObject("type", NullPointerException.getErrorTypeName());
+        mv.setViewName("error");
+        return mv;
     }
 }
